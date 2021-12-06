b79063c53a9cbe966cd224e8264d3af16c67bfa7
miablo <mvd5044@psu.edu>
2021-12-05 20:49:36 -0500

    part 2 // completed

--- src/Main.java
+++ src/Main.java
@@ -23,10 +23,6 @@ public class Main {
         Read reader = new Read();
         movement = reader.readMeasurement();
         String []direction;
-
-        int horizontal, depth;
-        horizontal = 0;
-        depth = 0;
         // Your horizontal position and depth both start at 0.
         // The steps above would then modify them as follows:
         //
@@ -36,17 +32,51 @@ public class Main {
         //up 3 decreases your depth by 3, resulting in a value of 2.
         //down 8 adds 8 to your depth, resulting in a value of 10.
         //forward 2 adds 2 to your horizontal position, a total of 15.
+        int horizontal, depth;
+        horizontal = 0;
+        depth = 0;
+
+        //down X increases your aim by X units.
+        //up X decreases your aim by X units.
+        //forward X does two things:
+        //   1. It increases your horizontal position by X units.
+        //   2. It increases your depth by your aim multiplied by X.
+
+        int aim = 0;
+
+        // Now, the above example does something different:
+        //
+        //forward 5 adds 5 to your horizontal position, a total of 5.
+        // Because your aim is 0, your depth does not change.
+
+        //down 5 adds 5 to your aim, resulting in a value of 5.
+        //forward 8 adds 8 to your horizontal position, a total of 13.
+        // Because your aim is 5, your depth increases by 8*5=40.
+
+        //up 3 decreases your aim by 3, resulting in a value of 2.
+        //down 8 adds 8 to your aim, resulting in a value of 10.
+
+        //forward 2 adds 2 to your horizontal position, a total of 15.
+        // Because your aim is 10, your depth increases by 2*10=20 to a total of 60.
+
+        //After following these new instructions, you would have a horizontal position of 15 and a depth of 60. (Multiplying these produces 900.)
+        //
+        //Using this new interpretation of the commands, calculate the horizontal position and depth you would have after following the planned course.
+        //
+        //
+        // What do you get if you multiply your final horizontal position by your final depth?
         for (String s : movement) {
             direction = s.split(" ", 2);
             switch (direction[0]) {
                 case "forward":
                     horizontal += Integer.parseInt(direction[1]);
+                    depth+=Integer.parseInt(direction[1])*aim;
                     break;
                 case "down":
-                    depth += Integer.parseInt(direction[1]);
+                    aim += Integer.parseInt(direction[1]);
                     break;
                 case "up":
-                    depth -= Integer.parseInt(direction[1]);
+                    aim -= Integer.parseInt(direction[1]);
                     break;
 
                 default:
@@ -71,8 +101,6 @@ public class Main {
 
         System.out.println("Final Multiple: " + horizontal*depth);
     }
-
-
 
 
 }
